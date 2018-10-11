ea426606ac5ce406fd8bba1885f3014557cf0dca
Fabian Te <fabiante@users.noreply.github.com>
2018-10-11 22:11:06 +0200

    Removed debugging timeout

--- start.sh
+++ start.sh
@@ -8,8 +8,5 @@ mkdir ark
 echo "Trying to install / update ARK..."
 ./steamcmd +login anonymous +force_install_dir /usr/games/ark +app_update 376030 +quit
 
-echo "Timeout 3 min..."
-sleep 2000000000
-
 echo "Starting server..."
 /usr/games/ark/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=example -server -log
