0fba3e712b57fddf9f0f809af886f4edf9ffcb8c
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-10-18 19:56:03 +0200

    Add shared volume

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 cd /usr/games/
 
 # Install Ark
-echo "Creating folder for ARK installation"
+echo "Creating folder for ARK installation..."
 mkdir ark
 echo "Trying to install / update ARK..."
 ./steamcmd +login anonymous +force_install_dir /usr/games/ark +app_update 376030 +quit
 
 echo "Starting server..."
-/usr/games/ark/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=example -server -log
+/usr/games/ark/ShooterGame/Binaries/Linux/ShooterGameServer Ragnarok?listen?SessionName=YourServerName?ServerPassword=PasswordForJoin?ServerAdminPassword=PasswordForAdmin -server -log
